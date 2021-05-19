0fe142c0d3d25c936ae4cde81d8e80713e6954de
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-19 14:30:55 +0300

    add express session and passport to package.json initialize and require passport and express session

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -13,8 +15,26 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//set up passport
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//finished setting up passport
+
+
 app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
+  res.render(process.cwd() + '/views/pug/index', { 
+    title: 'Hello', 
+    message: 'Please login' 
+  });
 });
 
 const PORT = process.env.PORT || 3000;
