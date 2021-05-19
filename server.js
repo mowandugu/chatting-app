59647d139a457cd73583e6b52ee970e38ce68cd4
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-19 16:27:55 +0300

    set up database lifecycle functionality

--- server.js
+++ server.js
@@ -32,25 +32,64 @@ app.use(passport.session());
 //finished setting up passport
 
 
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', { 
-    title: 'Hello', 
-    message: 'Please login' 
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  // Be sure to add this...
+}).catch((e) => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+// app.listen out here...
 
-//Serialize user object
+// app.route('/').get((req, res) => {
+//   res.render(process.cwd() + '/views/pug/index', { 
+//     title: 'Hello', 
+//     message: 'Please login' 
+//   });
+// });
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  done(null, null);
-  // });
-});
 
-//end of serialization
+
+// //Serialize user object
+
+// passport.serializeUser((user, done) => {
+//   done(null, user._id);
+// });
+// passport.deserializeUser((id, done) => {
+//   myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+//   done(null, null);
+//   });
+// });
+
+//   // Be sure to add this...
+// }).catch((e) => {
+//   app.route('/').get((req, res) => {
+//     res.render('pug', { title: e, message: 'Unable to login' });
+//   });
+// });
+// // app.listen out here...
+
+// //end of serialization
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
