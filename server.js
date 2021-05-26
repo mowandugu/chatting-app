eb83c6dccb44daaa11fce75a83f18cb913e1252c
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-26 11:32:18 +0300

    register process for new user

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ myDB(async (client) => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -60,6 +61,35 @@ myDB(async (client) => {
     req.logout();
     res.redirect('/');
   });
+
+
+  //Registration of New Users
+
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              next(null, doc.ops[0]);
+            }
+          });
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+
 
   app.use((req, res, next) => {
     res.status(404).type('text').send('Not Found');
