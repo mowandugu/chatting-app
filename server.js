df0ef4edb098bda61fe421d393e88a013f82c3db
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-26 12:02:03 +0300

    add bcrypt and authentication strategy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
 const LocalStrategy = require('passport-local');
-
+const bcrypt = require('bcrypt');
 
 
 const app = express();
@@ -67,13 +67,14 @@ myDB(async (client) => {
 
   app.route('/register').post(
     (req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12); //hashing password
       myDataBase.findOne({ username: req.body.username }, function (err, user) {
         if (err) {
           next(err);
         } else if (user) {
           res.redirect('/');
         } else {
-          myDataBase.insertOne({ username: req.body.username, password: req.body.password }, (err, doc) => {
+          myDataBase.insertOne({ username: req.body.username, password: hash }, (err, doc) => {
             if (err) {
               res.redirect('/');
             } else {
@@ -111,7 +112,9 @@ myDB(async (client) => {
         console.log('User '+ username +' attempted to log in.');
         if (err) { return done(err); }
         if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
+        if (!bcrypt.compareSync(password, user.password)) { 
+          return done(null, false);
+        }
         return done(null, user);
       });
     }
