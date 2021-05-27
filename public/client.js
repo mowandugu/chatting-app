5116463f3bdbd204b83214b3f4749a26e697b596
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-27 13:25:59 +0300

    add user count to socket listener and emitter

--- public/client.js
+++ public/client.js
@@ -1,13 +1,16 @@
-// This file's full path is /public/client.js
 $(document).ready(function () {
-  /* Global io */
+  /* Global io*/
   let socket = io();
-  
+
+  socket.on('user count', function (data) {
+    console.log(data);
+  });
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
-      let messageToSend = $('#m').val();
+    let messageToSend = $('#m').val();
     // Send message to server here?
-      $('#m').val('');
+    $('#m').val('');
     return false; // Prevent form submit from refreshing page
   });
 });
