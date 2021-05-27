c362b72c864c3598cca0193f2773a890d8f3e397
user <ygit config --global user.emaimowandugu@gmail.comgit config --global user.email>
2021-05-27 17:24:37 +0300

    updated callback url

--- auth.js
+++ auth.js
@@ -29,7 +29,7 @@ module.exports = function (app, myDataBase) {
     passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: 'https://chattingapp1234.herokuapp.com//auth/github/callback'
+        callbackURL: 'https://chattingapp1234.herokuapp.com/auth/github/callback'
     },
     function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
